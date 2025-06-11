02fc424355ba3da6e0306871b0a25c65f729b2bb
psvm203 <psvm203@proton.me>
2025-06-11 10:09:54 +0900

    Move data to yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use serde::Deserialize;
+use std::collections::HashMap;
 use yew::prelude::*;
 
-fn generate_job(job_name: &str, image_name: &str) -> Html {
-    let image_source = format!("assets/{image_name}.png");
+#[derive(Deserialize)]
+#[allow(unused)]
+struct Skill {
+    name: String,
+    src: String,
+    modifier: String,
+    tag: Option<Vec<String>>,
+}
+
+#[derive(Deserialize)]
+struct Job {
+    name: String,
+    src: String,
+    #[allow(unused)]
+    skills: Option<Vec<Skill>>,
+}
+
+fn generate_job_card(job_name: &str, image_name: &str) -> Html {
+    let image_source = format!("assets/jobs/{image_name}.png");
 
     html! {
         <div class={"card w-32 bg-base-200 border-1 items-center rounded-box shadow-sm "}>
@@ -17,32 +36,19 @@ fn generate_job(job_name: &str, image_name: &str) -> Html {
 
 #[function_component]
 fn App() -> Html {
-    let warriors_data = [
-        ("히어로", "hero"),
-        ("팔라딘", "paladin"),
-        ("다크나이트", "dark_knight"),
-        ("소울마스터", "soul_master"),
-        ("미하일", "mihile"),
-        ("블래스터", "blaster"),
-        ("데몬 슬레이어", "demon_slayer"),
-        ("데몬 어벤져", "demon_avenger"),
-        ("아란", "aran"),
-        ("카이저", "kaiser"),
-        ("아델", "adele"),
-        ("제로", "zero"),
-        ("렌", "len"),
-    ];
+    let data = include_str!("data.yaml");
+    let classes: HashMap<String, Vec<Job>> = serde_yaml::from_str(data).unwrap();
 
-    let mut warriors = vec![];
+    let mut warrior_cards = vec![];
 
-    for &(job_name, image_name) in &warriors_data {
-        let job = generate_job(job_name, image_name);
-        warriors.push(job);
+    for job in &classes["전사"] {
+        let card = generate_job_card(&job.name, &job.src);
+        warrior_cards.push(card);
     }
 
     let characters = html! {
         <div class={"grid grid-cols-5 gap-4"}>
-            {for warriors}
+            {for warrior_cards}
         </div>
     };
 
